94679ed592bee16997827f17b2423ad987d7eeff
Deivid Silva <imdeividsilva@gmail.com>
2023-09-30 16:01:46 -0300

    criação do testData

--- src/components/WeightHistory/WeightHistoryTable.tsx
+++ src/components/WeightHistory/WeightHistoryTable.tsx
@@ -1,15 +1,6 @@
-const WeightHistoryTable = () => {
+import { testWeights } from 'testData'
 
-	const testData = [
-		{ id: '2023-09-01', weight: 70 },
-		{ id: '2023-09-02', weight: 70 },
-		{ id: '2023-09-03', weight: 70 },
-		{ id: '2023-09-04', weight: 70 },
-		{ id: '2023-09-05', weight: 70 },
-		{ id: '2023-09-06', weight: 70 },
-		{ id: '2023-09-07', weight: 70 },
-		{ id: '2023-09-08', weight: 70 }
-	]
+const WeightHistoryTable = () => {
 
 	return (
 		<table>
@@ -20,7 +11,7 @@ const WeightHistoryTable = () => {
 				</tr>
 			</thead>
 			<tbody>
-				{testData.map(data => {
+				{testWeights.map(data => {
 					return (
 						<tr>
 							<td>{data.id}</td>
---
+++ src/testData.ts
@@ -0,0 +1,10 @@
+export const testWeights = [
+	{ id: '2023-09-01', weight: 70 },
+	{ id: '2023-09-02', weight: 70 },
+	{ id: '2023-09-03', weight: 70 },
+	{ id: '2023-09-04', weight: 70 },
+	{ id: '2023-09-05', weight: 70 },
+	{ id: '2023-09-06', weight: 70 },
+	{ id: '2023-09-07', weight: 70 },
+	{ id: '2023-09-08', weight: 70 }
+]
