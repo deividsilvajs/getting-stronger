47fac5a0e9e21c033ecbce9b1f3681667b27c94f
Deivid Silva <imdeividsilva@gmail.com>
2023-10-01 11:14:53 -0300

    responsividade a tabela de pesos

--- src/components/WeightHistory/WeightHistoryTable.tsx
+++ src/components/WeightHistory/WeightHistoryTable.tsx
@@ -3,24 +3,26 @@ import { testWeights } from 'testData'
 const WeightHistoryTable = () => {
 
 	return (
-		<table>
-			<thead>
-				<tr>
-					<th>Data</th>
-					<th>Peso</th>
-				</tr>
-			</thead>
-			<tbody>
-				{testWeights.map(data => {
-					return (
-						<tr>
-							<td>{data.id}</td>
-							<td>{data.weight}</td>
-						</tr>
-					)
-				})}
-			</tbody>
-		</table>
+		<div className='table-container'>
+			<table>
+				<thead>
+					<tr>
+						<th>Data</th>
+						<th>Peso</th>
+					</tr>
+				</thead>
+				<tbody>
+					{testWeights.map(data => {
+						return (
+							<tr>
+								<td>{data.id}</td>
+								<td>{data.weight}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</div>
 	)
 
 }
