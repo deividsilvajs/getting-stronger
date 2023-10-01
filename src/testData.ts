3bf9920f87038768eeab53c20b6a96ce1585c3b4
Deivid Silva <imdeividsilva@gmail.com>
2023-10-01 19:52:16 -0300

    reestilização

--- src/testData.ts
+++ src/testData.ts
@@ -1,9 +1,7 @@
 export const testWeights = [
 	{ id: '2023-09-01', weight: 50 },
-	{ id: '2023-09-05', weight: 50.5 },
-	{ id: '2023-09-10', weight: 51 },
-	{ id: '2023-09-15', weight: 51.5 },
-	{ id: '2023-09-20', weight: 52 },
-	{ id: '2023-09-25', weight: 52.5 },
-	{ id: '2023-09-30', weight: 53 }
+	{ id: '2023-09-08', weight: 50.5 },
+	{ id: '2023-09-15', weight: 51 },
+	{ id: '2023-09-22', weight: 51.5 },
+	{ id: '2023-09-29', weight: 52 },
 ]
