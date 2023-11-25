885ba38e2a6937b6b174549e92ea50e54c0454f3
Deivid Silva <imdeividsilva@gmail.com>
2023-11-25 12:25:34 -0300

    criação do design inicial do RecordWeightForm

--- src/components/WeightHistory/RecordWeightForm.tsx
+++ src/components/WeightHistory/RecordWeightForm.tsx
@@ -1,7 +1,19 @@
 const RecordWeightForm = () => {
 
 	return (
-		<h1>RecordWeightForm</h1>
+		<div className='form-container'>
+			<div className='card-container'>
+				<h1 style={{marginBottom: 12}}>Registrar Peso</h1>
+				<form>
+					<input className='info-input' type='text' placeholder='Digite o peso' />
+					<input className='info-input' type='date' />
+					<div style={{marginBottom: 8}}>
+						<input type='submit' value='Registrar' />
+						<input type='button' value='Cancelar' />
+					</div>
+				</form>
+			</div>
+		</div>
 	)
 
 }
