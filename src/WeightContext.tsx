6667cb4ba31ec8eaed0e00c58a250defa9e1ca2a
Deivid Silva <imdeividsilva@gmail.com>
2023-11-28 10:13:49 -0300

    criação do testWeights

--- src/WeightContext.tsx
+++ src/WeightContext.tsx
@@ -1,5 +1,8 @@
 import { createContext, useState } from 'react'
+
 import { Weights, WeightsState } from 'types/weight'
+
+import { weights as testWeights } from 'testWeights'
 
 const DEFAULT_VALUE: WeightsState = [
     [{ id: '', weight: 0 }],
@@ -14,7 +17,7 @@ interface WeightsProviderProps {
 
 const WeightsProvider = ({ children }: WeightsProviderProps) => {
 
-    const [weights, setWeights] = useState<Weights>([])
+    const [weights, setWeights] = useState<Weights>(testWeights)
 
     return (
         <WeightsContext.Provider value={[weights, setWeights]}>
---
+++ src/testWeights.ts
@@ -0,0 +1,16 @@
+import { Weights } from 'types/weight'
+
+export const weights: Weights = [
+    { id: '2023-01-01', weight: 60 },
+    { id: '2023-01-02', weight: 61 },
+    { id: '2023-01-03', weight: 62 },
+    { id: '2023-01-04', weight: 63 },
+    { id: '2023-01-05', weight: 64 },
+    { id: '2023-01-06', weight: 65 },
+    { id: '2023-01-07', weight: 66 },
+    { id: '2023-01-08', weight: 67 },
+    { id: '2023-01-09', weight: 68 },
+    { id: '2023-01-10', weight: 69 },
+    { id: '2023-01-11', weight: 70 },
+    { id: '2023-01-12', weight: 71 }
+]
